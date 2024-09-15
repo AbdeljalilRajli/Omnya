bdb2046b824e6e3c2f6a8317557f40f79b747b7b
Abdeljalil Rajli <rajli.contact@gmail.com>
2024-09-15 22:44:21 +0100

    change academics image

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "218f025a4bb32fc0"; }
+/******/ 		__webpack_require__.h = function() { return "886548d10dabdc4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
